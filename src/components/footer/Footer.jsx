5de9a79cd7b984ce4ac354a050160d405622fbe2
Povilas <jakimaviciuspovilas@gmail.com>
2023-01-16 17:01:46 +0200

    footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,33 @@
+import Icon from '../UI/Icon';
+import css from './Footer.module.css';
+function Footer(props) {
+  return (
+    <div className={css.footer}>
+      <div className={`${css.footerDivide} container`}>
+        <div className={css.footerText}>
+          <h3>Like what you see?</h3>
+          <p>Contact me if you have a job offer</p>
+        </div>
+        <ul>
+          <li>
+            <Icon iconName="phone" yellow />
+            <p>+37063672327</p>
+          </li>
+          <li>
+            <Icon iconName="envelope-o" yellow />
+            <p>jakimaviciuspovilas@gmail.com</p>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/povilas-jakimavičius-62a11325b"
+              target="_blank"
+            >
+              <Icon iconName="linkedin" yellow />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default Footer;
