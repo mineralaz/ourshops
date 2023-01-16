8b8e23781ad02bccb9319cc78962bf6363c2a5bd
Povilas <jakimaviciuspovilas@gmail.com>
2023-01-16 17:03:22 +0200

    Page titles removed

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,7 +4,6 @@ import PageTitle from '../components/UI/PageTitle';
 function LoginPage(props) {
   return (
     <div className="container">
-      <PageTitle>Login</PageTitle>
       <LoginAuthForm />
     </div>
   );
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -4,7 +4,6 @@ import PageTitle from '../components/UI/PageTitle';
 function RegisterPage(props) {
   return (
     <div className="container">
-      <PageTitle>Register</PageTitle>
       <RegisterAuthForm />
     </div>
   );
