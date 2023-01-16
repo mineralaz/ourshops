a074827d34b2843b14b98644a74681e67a206be1
Povilas <jakimaviciuspovilas@gmail.com>
2023-01-16 17:03:09 +0200

    footer in homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
+import Footer from '../components/footer/Footer';
 import PageTitle from '../components/UI/PageTitle';
 
 function HomePage(props) {
   return (
-    <div className="container">
-      <PageTitle>Welcome to our sponsored shops</PageTitle>
-    </div>
+    <>
+      <div className="container">
+        <PageTitle>Welcome to our sponsored shops</PageTitle>
+      </div>
+      <Footer />
+    </>
   );
 }
 export default HomePage;
