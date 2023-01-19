19bcc502cf4c8635073bc649efac12a5c02d6a4e
Povilas <jakimaviciuspovilas@gmail.com>
2023-01-19 13:44:30 +0200

    addshoppage bg

--- src/pages/AddShopPage.jsx
+++ src/pages/AddShopPage.jsx
@@ -1,16 +1,17 @@
 import Footer from '../components/footer/Footer';
 import AddShopForm from '../components/shops/AddShopForm';
 import PageTitle from '../components/UI/PageTitle';
+import css from './AddShopPage.module.css';
 
 function AddShopPage(props) {
   return (
-    <>
+    <div className={css.addShopPage}>
       <div className="container vh90">
         <PageTitle>Add your shop to our list</PageTitle>
         <AddShopForm />
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
 export default AddShopPage;
