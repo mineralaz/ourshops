14cb16e727502e43a42f7c7880f6c3ab7db62324
Povilas <jakimaviciuspovilas@gmail.com>
2023-01-19 12:05:06 +0200

    homepage bg

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,15 @@
 import Footer from '../components/footer/Footer';
 import PageTitle from '../components/UI/PageTitle';
+import css from './HomePage.module.css';
 
 function HomePage(props) {
   return (
-    <>
+    <div className={css.homePage}>
       <div className="container vh90">
         <PageTitle>Welcome to our sponsored shops</PageTitle>
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
 export default HomePage;
